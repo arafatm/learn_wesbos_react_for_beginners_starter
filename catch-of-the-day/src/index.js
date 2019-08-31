a806486f77e84685d25531433216505ffea8d0dd
Arafat M <github@arafatm.com>
2019-08-30 20:15:06 -0700

    `render` and attach to DOM

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'; // Can import from packages in package.json
+import { render } from 'react-dom'; // Only need render from this package
 
 class StorePicker extends React.Component { 
 
   render() { // Every React.Component requires a render()
     return <p>Hello!</p>
   }
-
 }
+
+render(<p>HEEEEYYYY!!!</p>, document.querySelector('#main'));
