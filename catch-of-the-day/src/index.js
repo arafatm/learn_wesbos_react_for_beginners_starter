fa5d7b2ee96abffb40aaca94e6ddecfcd8b3f5c4
Arafat M <github@arafatm.com>
2019-08-30 20:02:00 -0700

    Our first `React.Component` with required `render()` - note that nothing will happen because we haven't **mount**ed our component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,9 @@
-// let's go!
-alert("hi")
+import React from 'react'; // Can import from packages in package.json
+
+class StorePicker extends React.Component { 
+
+  render() { // Every React.Component requires a render()
+    return <p>Hello!</p>
+  }
+
+}
