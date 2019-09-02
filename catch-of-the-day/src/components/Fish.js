9a507def1b55e10741f38b0eb36e5a5473d70685
Arafat M <github@arafatm.com>
2019-09-02 15:27:53 -0700

    Display all Fish details

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -4,13 +4,16 @@ import { formatPrice } from '../helpers';
 class Fish extends React.Component {
 
   render() {
-    const { image, name, price, description } = this.props.details;
+    const { image, name, price, desc } = this.props.details;
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
-        <h3 className="fish-name">{name}
+        <h3 className="fish-name">
+          {name}
           <span className="price">{formatPrice(price)}</span> 
         </h3>
+        <p className="desc">{desc}</p>
+        <button>Add To Cart</button>
       </li> 
     );
   }
