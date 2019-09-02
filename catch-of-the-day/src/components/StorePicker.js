e7df66606230c9384aadab2bda1c167ea8371af8
Arafat M <github@arafatm.com>
2019-09-01 18:33:16 -0700

    Also cant return sibling elements

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,6 +4,7 @@ class StorePicker extends React.Component {
 
   render() { // Every React.Component requires a render()
     return (
+      <p>Fish!</p>
       <form action="" className="store-picker">
         <div>test</div>
       </form>
