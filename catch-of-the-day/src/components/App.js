e700f14e3070b565d7970ed6d2d820cd86f7352b
Arafat M <github@arafatm.com>
2019-09-01 20:56:25 -0700

    Pass final props to Header

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="This is a tagline" age={500} cool={true} />
+          <Header tagline="Fresh Seafood Market" />
         </div>
         <Inventory />
         <Order />
