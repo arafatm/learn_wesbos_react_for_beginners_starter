e1924e1a8b2ae887af36cf1d78867dfd504bd9df
Arafat M <github@arafatm.com>
2019-09-01 21:57:31 -0700

    :warning: Bad Practice. Example of click event handler

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,10 +3,14 @@ import { getFunName } from '../helpers.js';
 
 class StorePicker extends React.Component {
 
+  handleClick() {
+    alert("Heyyy!");
+  }
   render() { // Every React.Component requires a render()
     return (
       <form action="" className="store-selector">
         <h2>Please Enter A Store</h2>
+        <button onClick={this.handleClick}>Click me!</button>
         <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } />
         <button type="submit">Visit Store -></button>
       </form>
