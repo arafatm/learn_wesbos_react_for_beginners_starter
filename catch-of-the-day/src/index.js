83ee084a6cc150b455e0df92ac5886b5c3f50b70
Arafat M <github@arafatm.com>
2019-09-01 22:13:37 -0700

    Fix warning with App not referenced

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'; // Can import from packages in package.json
 import { render } from 'react-dom'; // Only need render from this package
 
-import App from './components/App';
 import Router from './components/Router';
 
 import "./css/style.css";
