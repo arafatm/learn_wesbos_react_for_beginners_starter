dcc70e0fa472ff6f8118716db67321b370d78db2
Arafat M <github@arafatm.com>
2019-09-02 13:21:25 -0700

    Added helpers for defaultValues on Inventory form

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { fishRand } from '../helpers.js'; 
 
 class AddFishForm extends React.Component {
   nameRef = React.createRef();
@@ -22,16 +23,18 @@ class AddFishForm extends React.Component {
   }
 
   render() {
+    const myFish = fishRand();
+
     return (
       <form className="fish-edit" onSubmit={this.createFish}>
-        <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
-        <input name="price" ref={this.priceRef} type="text" placeholder="Price" />
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name" defaultValue={myFish[0]} />
+        <input name="price" ref={this.priceRef} type="text" placeholder="Price" defaultValue={myFish[1]} />
         <select name="status" ref={this.statusRef} placeholder="Status">
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea name="desc" ref={this.descRef} placeholder="Desc"></textarea>
-        <input name="image" ref={this.imageRef} type="text" placeholder="Image" />
+        <textarea name="desc" ref={this.descRef} placeholder="Desc" defaultValue={myFish[2]}></textarea>
+        <input name="image" ref={this.imageRef} type="text" placeholder="Image" defaultValue={myFish[3]} />
         <button type="submit">+ Add Fish</button>
       </form>
     );
--- catch-of-the-day/src/helpers.js
+++ catch-of-the-day/src/helpers.js
@@ -1,3 +1,30 @@
+export function fishRand() {
+  const fish = fishRandName();
+  const desc = fish + " is a tasy fish";
+  const img = fish + " image here";
+  return([fish, fishRandPrice(), desc, img]);
+}
+export function fishRandName() {
+  const fish = [
+    "whale shark",
+    "alligator gar",
+    "spadefish",
+    "oarfish",
+    "rabbitfish",
+    "pollock",
+    "blueline tilefish",
+    "atlantic bonito",
+    "cleaner fish",
+    "scamp",
+    "damselfish",
+    "white grunt"
+  ];
+  return rando(fish);
+}
+export function fishRandPrice() {
+  return Math.floor(Math.random() * 10000)
+}
+
 export function formatPrice(cents) {
   return (cents / 100).toLocaleString("en-US", {
     style: "currency",
