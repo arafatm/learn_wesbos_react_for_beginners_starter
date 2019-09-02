bb0b9772eb61b10bae46101311b7c4725e1f977f
Arafat M <github@arafatm.com>
2019-09-01 19:25:01 -0700

    Header component with some pretty css

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -4,7 +4,21 @@ class Header extends React.Component {
 
   render() {
     return (
-      <div className="header">Header</div>
+      <React.Fragment>
+        <header className="top">
+          <h1>
+            Catch
+            <span className="ofThe">
+              <span className="of">of</span>
+              <span className="the">the</span>
+            </span>
+            Day
+          </h1>
+          <h3 className="tagline">
+            <span>Fresh Seafood Market</span>
+          </h3>
+        </header>
+      </React.Fragment>
     );
   }
 }
