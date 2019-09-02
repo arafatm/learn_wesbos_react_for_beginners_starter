a51b7df182230a80e6325a87b0676c8e90784b72
Arafat M <github@arafatm.com>
2019-09-02 13:45:41 -0700

    Refresh AddFishForm on Submit with `e.currentTarget.reset()`

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -20,6 +20,8 @@ class AddFishForm extends React.Component {
       image: this.imageRef.current.value,
     };
     this.props.addFish(fish);
+
+    e.currentTarget.reset(); // refresh the form
   }
 
   render() {
