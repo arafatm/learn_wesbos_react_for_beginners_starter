e487fc3d33f96ddfd7e2e95cb9e43a72f1c7d8ef
Arafat M <github@arafatm.com>
2019-09-02 15:23:02 -0700

    Set up Fish component using props

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,8 +3,14 @@ import React from 'react';
 class Fish extends React.Component {
 
   render() {
+    const { image, name, price, description } = this.props.details;
     return (
-      <div className="single-fish">🐟</div> 
+      <li className="menu-fish">
+        <img src={image} alt={name} />
+        <h3 className="fish-name">{name}
+          <span className="price">{price}</span> 
+        </h3>
+      </li> 
     );
   }
 }
