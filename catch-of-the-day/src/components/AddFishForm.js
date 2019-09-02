fa0d1046129f6bb4c0a8ee0e3857fa54c2c6c068
Arafat M <github@arafatm.com>
2019-09-02 12:43:24 -0700

    Completed Add Fish form

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -3,8 +3,16 @@ import React from 'react';
 class AddFishForm extends React.Component {
   render() {
     return (
-      <form>
-        <p>AddFishForm component</p>
+      <form class="fish-edit">
+        <input name="name" type="text" placeholder="Name" />
+        <input name="price" type="text" placeholder="Price" />
+        <select name="status" placeholder="Status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea name="desc" placeholder="Desc"></textarea>
+        <input name="image" type="text" placeholder="Image" />
+        <button type="submit">+ Add Fish</button>
       </form>
     );
   }
