60e4c1c6c90cb3f1a8b2929bd9c0b64c91e31a24
Arafat M <github@arafatm.com>
2019-09-01 18:30:56 -0700

    multiline fragment wrapped in `return ( ... )`

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,10 +3,11 @@ import React from 'react';
 class StorePicker extends React.Component { 
 
   render() { // Every React.Component requires a render()
-    return 
-    <form action="" className="store-picker">
-      <div>test</div>
-    </form>
+    return (
+      <form action="" className="store-picker">
+        <div>test</div>
+      </form>
+    )
   }
 }
 
