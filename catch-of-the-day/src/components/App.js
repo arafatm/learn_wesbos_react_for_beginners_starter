73c657d00fb7d2b4e38f7ef69681bebb3fbb8a91
Arafat M <github@arafatm.com>
2019-09-09 19:19:39 -0700

    Pass App.addtoOrder to Fish

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -41,12 +41,19 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(
-              key => <Fish key={key} details={this.state.fishes[key]} />
+            {Object.keys(this.state.fishes).map(key =>
+              <Fish
+                key={key} 
+                details={this.state.fishes[key]} 
+                addToOrder={this.addToOrder}
+                />
             )}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order 
+          fishes={this.state.fishes} 
+          order={this.state.order} 
+        />
         <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
