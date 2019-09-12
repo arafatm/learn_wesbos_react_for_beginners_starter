0ac2c16b0375fa63f4a42791141ca0123e26a82c
Arafat M <github@arafatm.com>
2019-09-11 20:47:08 -0700

    pass state.order to Order and display orderIds

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -3,9 +3,15 @@ import React from 'react';
 class Order extends React.Component {
 
   render() {
+
+    const orderIds = Object.keys(this.props.order)
     return (
       <div className="order-wrap">
         <h2>Order</h2>
+        {orderIds}
+        <ul>
+          <li></li>
+        </ul>
       </div> 
     );
   }
