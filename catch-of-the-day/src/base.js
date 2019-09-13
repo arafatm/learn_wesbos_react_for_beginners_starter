1dce9616014d690834e1f968bff580b925bdf1b9
Arafat M <github@arafatm.com>
2019-09-12 19:43:59 -0700

    import `re-base` and `firebase` libraries

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,6 @@
+import Rebase from 're-base'; // react-firebase state mirroring
+import firebase from 'firebase'; // firebase adapter
+
+// Initialize firebase
+const firebaseApp = firebase.initializeApp()
+
