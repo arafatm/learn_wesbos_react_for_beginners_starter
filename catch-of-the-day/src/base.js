0746fe9229fc86ecdc5608e2ef901b5545a5e5f6
Arafat M <github@arafatm.com>
2019-09-12 19:54:38 -0700

    Initialize firebase connection

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,6 +1,13 @@
 import Rebase from 're-base'; // react-firebase state mirroring
 import firebase from 'firebase'; // firebase adapter
 
-// Initialize firebase
-const firebaseApp = firebase.initializeApp()
+// Your web app's Firebase configuration
+// Copied from firebase console
+var firebaseConfig = {
+  apiKey: "AIzaSyBw7ZPtNgHBEqH3ggUsUduJMVv_0-rTcEw",
+  authDomain: "wesbos-react-for-beginne-3d560.firebaseapp.com",
+  databaseURL: "https://wesbos-react-for-beginne-3d560.firebaseio.com"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
