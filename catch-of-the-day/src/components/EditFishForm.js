aa90b28eb1658d7c1d4dc4a934a533badd3d9aee
Arafat M <github@arafatm.com>
2019-09-14 19:51:48 -0700

    Show fish value in the form

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -4,14 +4,14 @@ class EditFishForm extends React.Component {
   render() {
     return (
       <div className="fish-edit">
-        <input type="text" name="name" />
-        <input type="text" name="price" />
-        <select type="text" name="status">
+        <input type="text" name="name" value={this.props.fish.name} />
+        <input type="text" name="price" value={this.props.fish.price}  />
+        <select type="text" name="status" value={this.props.fish.status} >
           <option value="available">Fresh!</option>
-          <option value="available">Sold Out!</option>
+          <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea name="desc" />
-        <input type="text" name="image" />
+        <textarea name="desc" value={this.props.fish.desc}  />
+        <input type="text" name="image" value={this.props.fish.image}  />
       </div>
     )
   }
