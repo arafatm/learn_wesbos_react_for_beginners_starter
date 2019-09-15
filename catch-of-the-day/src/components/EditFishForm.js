06c35488de5a051f24b7210275bf443bcd6ac363
Arafat M <github@arafatm.com>
2019-09-14 19:44:03 -0700

    Set up the EditFishForm with actual fields

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 class EditFishForm extends React.Component {
   render() {
-    return <p>Edit Fish</p>;
+    return (
+      <div className="fish-edit">
+        <input type="text" name="name" />
+        <input type="text" name="price" />
+        <select type="text" name="status">
+          <option value="available">Fresh!</option>
+          <option value="available">Sold Out!</option>
+        </select>
+        <textarea name="desc" />
+        <input type="text" name="image" />
+      </div>
+    )
   }
 }
 
